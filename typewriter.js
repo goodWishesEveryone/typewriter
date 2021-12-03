3011ff8dbf5d215d42e298751e9c5a0f302a2606
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-12-03 01:46:31 -0500

    Added typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,6 +1,4 @@
-/* console.log will add a new line character (\n) to the end of the string. However, in Node we can use process.stdout.write instead to have more control and avoid automatically adding an extra "newline character" at the end each time.
-
-Replace the console.log with process.stdout.write and confirm our results.*/
+// console.log will add a new line character (\n) to the end of the string. However, in Node we can use process.stdout.write instead to have more control and avoid automatically adding an extra "newline character" at the end each time.
 
 const sentence = "hello there from lighthouse labs";
 let delay = 0;
